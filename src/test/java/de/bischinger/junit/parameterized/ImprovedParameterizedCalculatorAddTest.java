6bbbd894a49005b94cac29335896f8f96da89a24
Alexander Bischof <alexanderxbischof@gmail.com>
2015-04-23 13:28:54 +0200

    Fixed test

--- src/test/java/de/bischinger/junit/parameterized/ImprovedParameterizedCalculatorAddTest.java
+++ src/test/java/de/bischinger/junit/parameterized/ImprovedParameterizedCalculatorAddTest.java
@@ -15,7 +15,7 @@ public class ImprovedParameterizedCalculatorAddTest {
 
     @Parameters
     public static Iterable<Object[]> data() {
-        return Arrays.asList(new Object[][] { { 1, 2, 3 }, { -1, -2, -3 }, { Integer.MAX_VALUE, Integer.MAX_VALUE, -3 },
+        return Arrays.asList(new Object[][] { { 1, 2, 3 }, { -1, -2, -3 }, { Integer.MAX_VALUE, Integer.MAX_VALUE, -2 },
             { Integer.valueOf(1), Integer.valueOf(2), 3 } });
     }
 
